bb02af6c536fd14f66025ad486ed615d99cce8d6
rasmusolofzon <rasmus.olofzon@gmail.com>
2014-04-08 15:32:09 +0200

    Ny version av SeekerMissile.

--- piaette/src/shapes/SeekerMissile.java
+++ piaette/src/shapes/SeekerMissile.java
@@ -2,22 +2,23 @@ package shapes;
 
 import main.Main;
 
-import org.newdawn.slick.Animation;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.SpriteSheet;
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Circle;
 
 public class SeekerMissile {
-	private float direction,movementSpeed,height,width;
+	private float direction, movementSpeed, height, width;
 	public Color color;
-	public Animation missileAnimation;
+	public Shape circle;
+	//public Animation missileAnimation;
 	
 	private SeekerMissile(float x, float y) throws SlickException {
 		direction = 0f;
 		movementSpeed = 5; // Tweak
 		width = Main.width;
 		height = Main.height;
-		missileAnimation = new Animation(new SpriteSheet("animations/runningPlayer.png",64,64),250);
+		circle = new Circle(x, y, 50);
 	}
 	
 	public SeekerMissile(float x, float y,Color color) throws SlickException{
