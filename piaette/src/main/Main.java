893a289e1f4e96a76dcf8d5f6c0d68f17756540c
Tomas <dic12tha@student.lu.se>
2014-05-13 18:24:01 +0200

    Fixat så att den jäkla PNG WARN försvann nu iaf. Renare konsol åtminstone

--- piaette/src/main/Main.java
+++ piaette/src/main/Main.java
@@ -14,6 +14,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		try {
+			System.setProperty("org.newdawn.slick.pngloader", "false");
 			AppGameContainer app = new AppGameContainer(new GameStater("Pjätt"));
 			app.setDisplayMode((int) (width*scale), (int) (height*scale), false);
 			app.setTargetFrameRate(fpsLimit);
