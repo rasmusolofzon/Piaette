44642b83fa6242ee7c4288c8f6f329a4457cfb18
Oskar <Oskar@OskarsEnvy>
2014-05-13 15:16:29 +0200

    Lobby grejjor

---
+++ piaette/src/Client/LobbyToServer.java
@@ -0,0 +1,30 @@
+package Client;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class LobbyToServer extends Thread {
+	private OutputStream os;
+	private Socket socket;
+	private String msg;
+
+	public LobbyToServer(Socket socket) throws IOException {
+		os = socket.getOutputStream();
+		this.socket = socket;
+	}
+
+	public void setMessage(String msg) {
+		this.msg = msg;
+	}
+
+	public void run() {
+		try {
+			os.write(msg.getBytes());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
