cadd1a0ffdc04b94966e9ef586852fed3d9c4bb9
AAAton <anton@monokulturen.se>
2014-05-14 13:05:55 +0200

    LobbyClient

--- piaette/src/Client/Client.java
+++ piaette/src/Client/Client.java
@@ -1,5 +1,14 @@
 package Client;
 
-public class Client {
+import java.io.IOException;
 
+public class Client {
+	public static void main(String[] args){
+		try {
+			LobbyClient client = new LobbyClient("localhost",22222,"Sven");
+		} catch (NumberFormatException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- piaette/src/Client/LobbyClient.java
+++ piaette/src/Client/LobbyClient.java
@@ -9,42 +9,34 @@ public class LobbyClient {
 	private OutputStream outputStream;
 	private InputStream inputStream;
 	private int playerId;
-	private String playerName = "Sven";
+	private String playerName;
 	
-	public LobbyClient(String machine, int port) {
-
-		try {
+	public LobbyClient(String machine, int port, String playerName) throws IOException,NumberFormatException{
+		this.playerName = playerName;
 			Socket socket = new Socket(machine, port);
 			outputStream = socket.getOutputStream();
 			inputStream = socket.getInputStream();
 			
 			if(handshake()){
+				System.out.println("handshake succesful");
 				Thread waitForServer = new LobbyFromServer(socket);
 				waitForServer.start();
-				Thread waitForClient = new LobbyToServer(socket);
-				waitForClient.start();
 			}
 
-		} catch (IndexOutOfBoundsException e) {
-			System.out.println("You have to write both machine and port number");
-		} catch (NumberFormatException e) {
-			System.out.println("You suck");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 	
 	private boolean handshake() throws IOException{
 		String initMessage = receiveMessage();
-		if(!initMessage.equals("welcome")) return false;
+		if(!initMessage.startsWith("welcome")) return false;
 		sendMessage("playerName: "+playerName);
 		String idMessage = receiveMessage();
 		playerId = Integer.parseInt(idMessage.substring(10));
+		System.out.println("Received id "+playerId+" from the server");
 		return true;
 	}
 	
 	public void sendMessage(String msg) throws IOException {
-		outputStream.write((msg).getBytes());
+		outputStream.write((msg+'\n').getBytes());
 	}
 
 	public String receiveMessage() throws IOException{
@@ -54,13 +46,11 @@ public class LobbyClient {
 			readByte = inputStream.read();
 			char a = (char) readByte;
 			sb.append(a);
-			System.out.print(a);
 			if (a == '\n') {
 				readByte = -1;
 			}
 		} while (readByte != -1);
-
-		return sb.toString();
+		return sb.toString().trim();
 	}
 	
 	public static void startGame(){
