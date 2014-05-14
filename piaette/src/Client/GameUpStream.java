e097012a37ed9616d2101f201774b2244de663b9
AAAton <anton@monokulturen.se>
2014-05-14 14:01:49 +0200

    Bunch of new stuff

---
+++ piaette/src/Client/GameUpStream.java
@@ -0,0 +1,12 @@
+package Client;
+
+public class GameUpStream extends Thread {
+	public GameUpStream(){
+		//TODO
+	}
+
+	@Override
+	public void run(){
+		//TODO
+	}
+}
--- piaette/src/main/Utility.java
+++ piaette/src/main/Utility.java
@@ -1,6 +1,9 @@
-package menu;
+package main;
 
 import java.awt.Font;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import org.newdawn.slick.UnicodeFont;
 import org.newdawn.slick.font.effects.ColorEffect;
@@ -14,5 +17,23 @@ public class Utility {
 		font.addGlyphs("@");
 		font.getEffects().add(new ColorEffect(java.awt.Color.white));
 		return font;
+	}
+	
+	public static String receiveMessage(InputStream in) throws IOException {
+		int readByte;
+		StringBuilder sb = new StringBuilder();
+		do {
+			readByte = in.read();
+			char a = (char) readByte;
+			sb.append(a);
+			if (a == '\n') {
+				readByte = -1;
+			}
+		} while (readByte != -1);
+
+		return sb.toString().trim();
+	}
+	public static void sendMessage(OutputStream outputStream, String msg) throws IOException {
+		outputStream.write((msg+'\n').getBytes());
 	}
 }
