00f4cd6160df30d9186b500ea5da39e1cbd8ab2e
AAAton <anton@monokulturen.se>
2014-05-14 13:26:06 +0200

    MailBox made into observable

--- piaette/src/server/LobbyMailBox.java
+++ piaette/src/server/LobbyMailBox.java
@@ -1,8 +1,9 @@
 package server;
 
 import java.util.ArrayList;
+import java.util.Observable;
 
-public class LobbyMailBox {
+public class LobbyMailBox extends Observable{
 	ArrayList<ClientHandler> players;
 	private final int MAX_PLAYERS = 4;
 	public LobbyMailBox(){
@@ -11,17 +12,21 @@ public class LobbyMailBox {
 	
 	public synchronized int addClient(ClientHandler clientHandler) {
 		players.add(clientHandler);
+		notifyAll();
 		return players.size();
 	}
 	
 	public void removeClient(ClientHandler clientHandler){
 		players.remove(clientHandler);
+		notifyAll();
 	}
 	
 	public boolean isServerFull(){
 		return players.size()==MAX_PLAYERS;
 	}
 	
-	
+	public ArrayList<ClientHandler> getClients(){
+		return players;
+	}
 	
 }
