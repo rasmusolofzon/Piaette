199c5cad7694d4c7525945f3e2d84d6d93b9da55
Tomas <dic12tha@student.lu.se>
2014-05-16 14:15:14 +0200

    Uppdateringshastighet

--- piaette/src/server/GameServer.java
+++ piaette/src/server/GameServer.java
@@ -119,7 +119,7 @@ public class GameServer {
 			long lastSend = 0;
 			while (true) {
 				long now = System.currentTimeMillis();
-				if ((now - lastSend)>=200) {
+				if ((now - lastSend)>=50) {
 					ArrayList<PlayerDefinition> arrPlayers = new ArrayList<PlayerDefinition>(players);
 					String debugP = new ServerProtocol(SEQ,arrPlayers,2).toString();
 					byte[] sndTemp = debugP.getBytes();
